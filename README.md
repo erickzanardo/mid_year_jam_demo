e65609bc43d3e3c42430cb0d86f3f19781c09052
Erick Zanardo <erickzanardoo@gmail.com>
2022-08-19 16:33:24 -0300

    uploading code

---
+++ README.md
@@ -0,0 +1,6 @@
+# Mid Year Flame Jam Demo
+
+Small repository containing the sourcebase of the demo about mini sprite given on
+the Mid Year Flame Game Jam opening.
+
+![](screenshoot.png)
---
+++ lib/assets.dart
@@ -0,0 +1,4 @@
+const library = '''sprite_1|8,8;3,0;3,1;4,0;5,1;2,0;7,1;1,0;3,1;1,0;1,1;1,0;1,1;1,0;7,1;2,0;5,1;2,0;1,1;1,0;1,1;1,0;1,1;1,0;1,1;3,0;1,1;1,0;1,1;2,0
+sprite_2|8,8;8,1;1,0;3,1;1,0;2,1;2,0;3,1;1,0;2,1;10,0;4,1;4,0;4,1;1,0;2,1;6,0;2,1;8,0''';
+
+const map = '''[{"x":0,"y":2,"data":{"sprite":"sprite_2"}},{"x":2,"y":2,"data":{"sprite":"sprite_2"}},{"x":1,"y":2,"data":{"sprite":"sprite_2"}},{"x":3,"y":2,"data":{"sprite":"sprite_2"}},{"x":4,"y":2,"data":{"sprite":"sprite_2"}},{"x":5,"y":2,"data":{"sprite":"sprite_2"}},{"x":6,"y":2,"data":{"sprite":"sprite_2"}},{"x":7,"y":2,"data":{"sprite":"sprite_2"}},{"x":8,"y":2,"data":{"sprite":"sprite_2"}},{"x":9,"y":2,"data":{"sprite":"sprite_2"}},{"x":2,"y":1,"data":{"sprite":"sprite_1"}}]''';
